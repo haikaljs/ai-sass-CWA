3375a961fc1e3495455639249cc457b99e8f98d1
haikaljs <haikaljavascript@gmail.com>
2023-09-14 08:58:35 +0800

    Setup clerk authentication

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,6 +1,14 @@
+import { UserButton } from "@clerk/nextjs";
 
 const DashboardPage = () => {
-  return <div>DashboardPage (Protected)</div>;
+  return (
+    <div>
+      <p>DashboardPage (Protected)</p>
+      <div>
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
