ef85107340545c6656bfaa9776cc5f3b6db1bd30
haikaljs <haikaljavascript@gmail.com>
2023-09-14 15:42:07 +0800

    Sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,12 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 const DashboardPage = () => {
   return (
     <div>
       <p>DashboardPage (Protected)</p>
-      <div>
-        <UserButton afterSignOutUrl="/" />
-      </div>
     </div>
   );
 };
